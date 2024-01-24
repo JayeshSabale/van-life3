922a37d9d2d9bf945cbd16174276390a89f42fac
JayeshSabale <jayeshsabale22@gmail.com>
2024-01-24 18:27:11 +0530

    Nested Routing
    A separate component for Navbar is created and outlet is used for nested routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,25 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Vans from "./components/pages/Vans";
-import VanDetail from "./components/pages/VanDetail";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Vans from "./pages/Vans";
+import VanDetail from "./pages/VanDetail";
+import Layout from "./components/Layout";
 
-import "./server"
+import "./server";
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          #VanLife
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
-      </Routes>        
+      <Routes >
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
