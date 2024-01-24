8c690adbbfab15ae30f08ab3abbd22232227b2f5
JayeshSabale <jayeshsabale22@gmail.com>
2024-01-24 09:46:59 +0530

    Vans page and Route created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 
+import Vans from "./components/pages/Vans";
+
 function App() {
   return (
     <BrowserRouter>
@@ -13,12 +15,14 @@ function App() {
         </Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-      </Routes>
+        <Route path="/vans" element={<Vans />} />
+      </Routes>        
     </BrowserRouter>
   );
 }
